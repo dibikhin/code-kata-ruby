345df6a9db54057c04ccd1567649b39a24dc63d3
dibikhin <dibikhin@gmail.com>
2013-05-19 08:46:34 +0400

    finished version

---
+++ binary-chop-i-r.rb
@@ -0,0 +1,60 @@
+﻿require 'test/unit'
+
+# binary chop: itarative process, recursive calls
+ 
+class TestClass < Test::Unit::TestCase
+  def chop(num, array)
+    return -1 if array.empty?
+    return chop_iter(num, array, 0, array.size - 1)
+  end    
+  
+  def chop_iter(num, array, bottom, top)
+    return -1 if bottom > top 
+    
+    middle = (top + bottom) / 2
+    middle_num = array[middle]
+    
+    return middle if middle_num == num
+    return chop_iter(num, array, middle + 1, top) if middle_num < num            
+    return chop_iter(num, array, bottom, middle - 1) if num < middle_num    
+  end
+  
+  def test_chop
+    assert_equal(-1, chop(3, []))
+    assert_equal(-1, chop(3, [1]))
+    assert_equal(-1, chop(3, [4]))
+    assert_equal(0,  chop(1, [1]))
+    
+    assert_equal(0,  chop(1, [1, 3]))
+    assert_equal(1,  chop(3, [1, 3]))
+    assert_equal(-1, chop(4, [1, 3]))
+    assert_equal(-1, chop(0, [1, 3]))
+    
+    assert_equal(0,  chop(1, [1, 3, 5]))
+    assert_equal(1,  chop(3, [1, 3, 5]))
+    assert_equal(2,  chop(5, [1, 3, 5]))
+    assert_equal(-1, chop(0, [1, 3, 5]))
+    assert_equal(-1, chop(2, [1, 3, 5]))
+    assert_equal(-1, chop(4, [1, 3, 5]))
+    assert_equal(-1, chop(6, [1, 3, 5]))
+    
+    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+    
+    assert_equal(-1, chop(8, [1, 3, 5, 7, 9]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7, 9, 11]))
+    assert_equal(-1, chop(10, [1, 3, 5, 7, 9, 11]))
+    assert_equal(-1, chop(12, [1, 3, 5, 7, 9, 11]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7, 9, 11]))
+    
+    assert_equal(-1, chop(4, [1, 3, 5, 7, 9, 11, 13]))
+#    assert_equal(-1, chop(10_000_000, (0..9999999).to_a.select { |a| a % 2 == 1 }))
+  end
+end
