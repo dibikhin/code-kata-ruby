0282b375cef0cca89852eb515bed77e0868ea26e
dibikhin <dibikhin@yahoo.com>
2014-08-04 15:04:53 +0400

    spellchecker simplified, bloom-filter funcs moved to bitmap

---
+++ Kata Five/bloom-filter.rb
@@ -0,0 +1,47 @@
+﻿class BloomFilter
+	class BitMap
+		def initialize(file_name = nil)
+		end	
+
+		def self.compute(words)
+			bitmap = BitMap.new
+			words.each do |word|
+				word_hash = compute_hash(word)
+				bitmap.add(word_hash)
+			end
+			bitmap
+		end
+	
+		def load(file_name)
+		end
+
+		def save(file_name)
+		end
+
+		def contains?(word_hash)
+			true
+		end
+	end
+
+	# private
+	def self.compute_hash(word)
+	end
+
+	def load_dictionary(dictionary_file_name)
+		File.open(dictionary_file_name, 'r').read.lines.map{ |line| line.strip }
+	end
+
+	# public
+
+	def self.refresh_bitmap(dictionary_file_name, bitmap_file_name)
+		words = load_dictionary(dictionary_file_name)
+		bitmap = BitMap.compute(words)
+		bitmap.save(bitmap_file_name)
+	end
+
+	def self.check_word(word, bitmap_file_name)
+		bitmap = BitMap.new(bitmap_file_name)
+		word_hash = compute_hash(word)
+		bitmap.contains?(word_hash)
+	end
+end
---
+++ Kata Five/spellchecker.rb
@@ -0,0 +1,5 @@
+﻿require '.\bloom-filter'
+
+word, bitmap_file_name = ARGV[0], ARGV[1]
+
+puts BloomFilter.check_word(word, bitmap_file_name)
