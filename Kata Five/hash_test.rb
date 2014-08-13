3b68c5e5b4e2c333516d6cc79991891e0cfb52ca
dibikhin <dibikhin@yahoo.com>
2014-08-13 17:17:09 +0400

    tested anagrams & tested hash funcs

---
+++ Kata Five/hash_test.rb
@@ -0,0 +1,20 @@
+﻿require 'pp'
+
+dictionary_file_name = ARGV[0]
+
+pre_anagrams = {}
+hashes = []
+words = File.open(dictionary_file_name, 'r').read.lines
+words.each do |word|
+	stripped_word = word.strip
+	sorted_chars = stripped_word.downcase.chars.sort * ''
+	#pre_anagrams[sorted_chars] = {}	if pre_anagrams[sorted_chars].nil?
+	#pre_anagrams[sorted_chars][:word] = stripped_word
+	hash = stripped_word.bytes.reduce(:+)
+	#pre_anagrams[sorted_chars][:hash] = hash
+	hashes << hash
+end
+
+#pp pre_anagrams.sort_by { |_, v| v.size } #.to_a.last(5)
+
+pp hashes.uniq.sort.size
--- Kata Six/find_anagrams.rb
+++ Kata Six/find_anagrams.rb
@@ -3,11 +3,12 @@ require 'pp'
 dictionary_file_name = ARGV[0]
 
 pre_anagrams = {}
-words = File.open(dictionary_file_name, 'r').read.lines.map{ |line| line.strip }
+words = File.open(dictionary_file_name, 'r').read.lines
 words.each do |word|
-	sorted_chars = word.downcase.chars.sort * ''
+	stripped_word = word.strip
+	sorted_chars = stripped_word.downcase.chars.sort * ''
 	pre_anagrams[sorted_chars] = []	if pre_anagrams[sorted_chars].nil?
-	pre_anagrams[sorted_chars] << word
+	pre_anagrams[sorted_chars] << stripped_word
 end
 
-pp pre_anagrams.sort_by { |_, v| v.size }
+pp pre_anagrams.sort_by { |_, v| v.size } #.to_a.last(5)
