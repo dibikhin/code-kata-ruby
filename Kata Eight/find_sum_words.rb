f3af819bf114ae4307e11f90cebfdb534c1f3d3a
dibikhin <dibikhin@yahoo.com>
2014-08-14 14:51:59 +0400

    first test ok on 500 words list

---
+++ Kata Eight/find_sum_words.rb
@@ -0,0 +1,15 @@
+﻿require 'pp'
+
+dictionary_file_name = ARGV[0]
+
+sum_words = []
+words = File.open(dictionary_file_name, 'r').read.lines.uniq.map { |w| w.strip }
+words.each do |word|
+	words.each do |a_word|			
+		if words.include?(word + a_word)
+			sum_words << { (word + a_word) => [word, a_word] } 
+		end
+	end
+end
+
+pp sum_words
