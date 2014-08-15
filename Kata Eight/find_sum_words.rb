1ca40fc6c1b412b7b775cf4abb2219708597a53a
dibikhin <dibikhin@yahoo.com>
2014-08-15 21:07:53 +0400

    compositions tested

--- Kata Eight/find_sum_words.rb
+++ Kata Eight/find_sum_words.rb
@@ -6,17 +6,17 @@ end
 
 # con + vex => convex
 # here + by => hereby
-def make_composition(one, another)
-	{ (one + another) => [one, another] }
+def add_composition(one, another, compositions)
+	compositions[one + another] = [one, another]
 end
 
 def find_compositions(words)
 	counter = 1
-	compositions = []
+	compositions = {}
 	words.each do |first_word|
 		words.each do |second_word|
 			counter =+ counter + 1
-			compositions << make_composition(first_word, second_word) if words.include?(first_word + second_word)
+			add_composition(first_word, second_word, compositions) if words.include?(first_word + second_word)
 		end
 	end
 	puts counter
@@ -26,5 +26,25 @@ end
 puts Time.now
 dictionary_file_name = ARGV[0]
 words = read_dictionary(dictionary_file_name)
-pp find_compositions(words)
+pp find_compositions(words) #.sort_by { |k, v| v }
 puts Time.now
+
+# [["ago", 3],
+ # ["into", 4],
+ # ["away", 4],
+ # ["door", 4],
+ # ["some", 4],
+ # ["along", 5],
+ # ["heart", 5],
+ # ["maybe", 5],
+ # ["today", 5],
+ # ["become", 6],
+ # ["within", 6],
+ # ["without", 7],
+ # ["someone", 7],
+ # ["however", 7],
+ # ["nothing", 7],
+ # ["anything", 8],
+ # ["something", 9],
+ # ["everything", 10],
+ # ["understand", 10]]
---
+++ Kata Eight/test_dict.rb
@@ -0,0 +1,2 @@
+﻿dictionary_file_name = ARGV[0]
+puts File.open(dictionary_file_name, 'r').read.lines.uniq.map { |word| word.strip.size }.uniq.sort
