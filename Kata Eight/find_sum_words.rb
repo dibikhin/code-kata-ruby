e2b9e21635ec1599bb0ce1fa16024da09b03ecb9
dibikhin <dibikhin@yahoo.com>
2014-08-22 20:49:15 +0400

    init

--- Kata Eight/find_sum_words.rb
+++ Kata Eight/find_sum_words.rb
@@ -1,6 +1,6 @@
 ﻿require 'pp'
 require 'set'
-#require 'ruby-prof'
+require 'ruby-prof'
 
 def	read_dictionary(dictionary_file_name)
 	File.open(dictionary_file_name, 'r').read.lines.map { |word| word.strip }.uniq
@@ -10,7 +10,7 @@ def find_compositions(words)
 	counter = 1
 	compositions = {}
 	words_set = Set.new(words)
-	#RubyProf.start
+	RubyProf.start
 	words.combination(2).each do |comb|
 		counter += 2
 		left_right = comb[0] + comb[1]
@@ -18,7 +18,7 @@ def find_compositions(words)
 		compositions[left_right] = comb if words_set.include?(left_right)
 		compositions[right_left] = comb if words_set.include?(right_left)
 	end
-	#puts RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
+	puts RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
 	puts counter
 	compositions
 end
@@ -27,6 +27,6 @@ dictionary_file_name = ARGV[0]
 start_time = Time.now
 
 words = read_dictionary(dictionary_file_name)
-pp find_compositions(words.first(5000))
+pp find_compositions(words.first(1000))
 
 puts Time.now - start_time
