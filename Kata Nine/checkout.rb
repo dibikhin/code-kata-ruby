265696d1205261b70f23e2cebb9f3908fcfaa3c0
dibikhin <dibikhin@yahoo.com>
2014-08-23 21:02:42 +0400

    rules added

--- Kata Nine/checkout.rb
+++ Kata Nine/checkout.rb
@@ -1,5 +1,5 @@
 ﻿class SpecialPrice
-	attr_accessor :qty, :price_per_qty
+	attr_accessor :set_size, :price_per_set
 end
 
 class Rule
@@ -21,11 +21,11 @@ class CheckOut
 	private
 	def parse_rules(rules)
 		rule_a, rule_a_spec_price = Rule.new, SpecialPrice.new
-		rule_a_spec_price.qty, rule_a_spec_price.price_per_qty = 3, 130
+		rule_a_spec_price.set_size, rule_a_spec_price.price_per_set = 3, 130
 		rule_a.sku, rule_a.unit_price, rule_a.special_price = 'A', 50, rule_a_spec_price
 
 		rule_b, rule_b_spec_price = Rule.new, SpecialPrice.new
-		rule_b_spec_price.qty, rule_b_spec_price.price_per_qty = 2, 45
+		rule_b_spec_price.set_size, rule_b_spec_price.price_per_set = 2, 45
 		rule_b.sku, rule_b.unit_price, rule_b.special_price = 'B', 30, rule_b_spec_price
 
 		rule_с = Rule.new
@@ -34,7 +34,7 @@ class CheckOut
 		rule_d = Rule.new
 		rule_d.sku, rule_d.unit_price = 'D', 15
 
-		[]
+		[rule_a, rule_b, rule_c, rule_d]
 	end
 end
 
