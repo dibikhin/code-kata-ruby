f7cdb67ddf294017885b7adc3adbfa1c9a73abdf
dibikhin <dibikhin@yahoo.com>
2014-08-25 22:05:08 +0400

    map/reduce, class refactoring failed

--- Kata Nine/checkout.rb
+++ Kata Nine/checkout.rb
@@ -1,9 +1,7 @@
-﻿class SpecialPrice
-	attr_accessor :set_size, :price_per_set
-end
+﻿require 'yaml'
 
 class Rule
-	attr_accessor :sku, :unit_price, :special_price
+	attr_accessor :sku, :unit_price, :set_size, :price_per_set
 end
 
 class CheckOut
@@ -22,16 +20,14 @@ class CheckOut
 	
 	def compute_total(rules, sku_list)
 		total = 0
-		stats = @sku_list.chars.group_by(&:chr).map { |chr, all_chars| { chr => all_chars.size } }
-		stats.each do |stat|
-			stat.each do |sku, count|
-				rule = @rules[sku]
-				if !rule.special_price.nil?
-					total += count / rule.special_price.set_size * rule.special_price.price_per_set
-					total += count % rule.special_price.set_size * rule.unit_price
-				else
-					total += count * rule.unit_price
-				end
+		stats = @sku_list.chars.group_by(&:chr).map { |chr, all_chars| { chr => all_chars.size } }.reduce(Hash.new, :merge)
+		stats.each do |sku, count|
+			rule = @rules[sku]
+			if !rule.set_size.nil? && !rule.price_per_set.nil?
+				total += count / rule.set_size * rule.price_per_set
+				total += count % rule.set_size * rule.unit_price
+			else
+				total += count * rule.unit_price
 			end
 		end
 		total
@@ -39,13 +35,13 @@ class CheckOut
 	
 	private
 	def parse_rules(rules)
-		rule_a, rule_a_spec_price = Rule.new, SpecialPrice.new
-		rule_a_spec_price.set_size, rule_a_spec_price.price_per_set = 3, 130
-		rule_a.sku, rule_a.unit_price, rule_a.special_price = 'A', 50, rule_a_spec_price
+		rule_a = Rule.new
+		rule_a.sku, rule_a.unit_price = 'A', 50
+		set_size, price_per_set = 3, 130
 
-		rule_b, rule_b_spec_price = Rule.new, SpecialPrice.new
-		rule_b_spec_price.set_size, rule_b_spec_price.price_per_set = 2, 45
-		rule_b.sku, rule_b.unit_price, rule_b.special_price = 'B', 30, rule_b_spec_price
+		rule_b = Rule.new
+		rule_b.sku, rule_b.unit_price = 'B', 30
+		set_size, price_per_set = 2, 45
 
 		rule_c = Rule.new
 		rule_c.sku, rule_c.unit_price = 'C', 20
@@ -53,14 +49,28 @@ class CheckOut
 		rule_d = Rule.new
 		rule_d.sku, rule_d.unit_price = 'D', 15
 
-		{ rule_a.sku => rule_a, rule_b.sku => rule_b, rule_c.sku => rule_c, rule_d.sku => rule_d }
+		a = { rule_a.sku => rule_a, rule_b.sku => rule_b, rule_c.sku => rule_c, rule_d.sku => rule_d }
+		#puts a.to_yaml
+		a
 	end
 end
 
-# Item  Unit      Special
-# 		Price     Price
-# --------------------------
-# A     50       3 for 130
-# B     30       2 for 45
-# C     20
-# D     15
+# ---
+# A: !ruby/object:Rule
+  # sku: A
+  # unit_price: 50
+  # special_price: !ruby/object:SpecialPrice
+    # set_size: 3
+    # price_per_set: 130
+# B: !ruby/object:Rule
+  # sku: B
+  # unit_price: 30
+  # special_price: !ruby/object:SpecialPrice
+    # set_size: 2
+    # price_per_set: 45
+# C: !ruby/object:Rule
+  # sku: C
+  # unit_price: 20
+# D: !ruby/object:Rule
+  # sku: D
+  # unit_price: 15
