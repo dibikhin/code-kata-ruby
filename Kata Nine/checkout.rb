97fe6366ba4f3948880eb42e61907958f010d11f
dibikhin <dibikhin@yahoo.com>
2014-08-26 21:44:13 +0400

    yaml rules parsing added ok

--- Kata Nine/checkout.rb
+++ Kata Nine/checkout.rb
@@ -35,42 +35,6 @@ class CheckOut
 	
 	private
 	def parse_rules(rules)
-		rule_a = Rule.new
-		rule_a.sku, rule_a.unit_price = 'A', 50
-		set_size, price_per_set = 3, 130
-
-		rule_b = Rule.new
-		rule_b.sku, rule_b.unit_price = 'B', 30
-		set_size, price_per_set = 2, 45
-
-		rule_c = Rule.new
-		rule_c.sku, rule_c.unit_price = 'C', 20
-
-		rule_d = Rule.new
-		rule_d.sku, rule_d.unit_price = 'D', 15
-
-		a = { rule_a.sku => rule_a, rule_b.sku => rule_b, rule_c.sku => rule_c, rule_d.sku => rule_d }
-		#puts a.to_yaml
-		a
+		YAML.load(rules)
 	end
 end
-
-# ---
-# A: !ruby/object:Rule
-  # sku: A
-  # unit_price: 50
-  # special_price: !ruby/object:SpecialPrice
-    # set_size: 3
-    # price_per_set: 130
-# B: !ruby/object:Rule
-  # sku: B
-  # unit_price: 30
-  # special_price: !ruby/object:SpecialPrice
-    # set_size: 2
-    # price_per_set: 45
-# C: !ruby/object:Rule
-  # sku: C
-  # unit_price: 20
-# D: !ruby/object:Rule
-  # sku: D
-  # unit_price: 15
--- Kata Nine/test_rules.rb
+++ Kata Nine/test_rules.rb
@@ -2,7 +2,33 @@
 require './checkout'
 
 class TestPrice < Minitest::Test
-  RULES = ""
+
+	# Item  Unit      Special
+	# 		Price     Price
+	# --------------------------
+	# A     50       3 for 130
+	# B     30       2 for 45
+	# C     20
+	# D     15
+
+  RULES =
+"---
+A: !ruby/object:Rule
+  sku: A
+  unit_price: 50
+  set_size: 3
+  price_per_set: 130
+B: !ruby/object:Rule
+  sku: B
+  unit_price: 30
+  set_size: 2
+  price_per_set: 45
+C: !ruby/object:Rule
+  sku: C
+  unit_price: 20
+D: !ruby/object:Rule
+  sku: D
+  unit_price: 15"
 	
   def price(goods)
     co = CheckOut.new(RULES)
@@ -38,11 +64,3 @@ class TestPrice < Minitest::Test
     co.scan("B");  assert_equal(175, co.total)
   end
 end
-
-# Item  Unit      Special
-# 		Price     Price
-# --------------------------
-# A     50       3 for 130
-# B     30       2 for 45
-# C     20
-# D     15
