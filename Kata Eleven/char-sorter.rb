ed464418e8650bda9c75abce4d4641fdaf43a8f7
dibikhin <dibikhin@yahoo.com>
2014-08-30 21:33:13 +0400

    sorting chars done ok

---
+++ Kata Eleven/char-sorter.rb
@@ -0,0 +1,11 @@
+﻿class CharSorter
+	def self.sort(str)
+		index = {}
+		('a'..'z').each { |chr| index[chr] = 0 }
+		str.downcase!
+		str.chars.each { |chr| index[chr] += 1 if !index[chr].nil?}
+		sorted_chars = []
+		index.each { |chr, count| sorted_chars << chr * count if count > 0 }
+		sorted_chars * ''
+	end
+end
---
+++ Kata Eleven/sorting_chars.rb
@@ -0,0 +1,10 @@
+﻿require 'minitest/autorun'
+require '.\char-sorter'
+
+class TestRack < Minitest::Test
+	def test_sort
+		expected = 'aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy'
+		str = 'When not studying nuclear physics, Bambi likes to play beach volleyball.'
+		assert_equal(expected, CharSorter.sort(str))		
+	end
+end
