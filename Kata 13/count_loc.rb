a5d750e974a7af69496ea0dce71f6d2300f720dd
dibikhin <dibikhin@yahoo.com>
2014-08-31 21:46:53 +0400

    kata 13 init

---
+++ Kata 13/count_loc.rb
@@ -0,0 +1,12 @@
+﻿str = "// This file contains 3 lines of code
+public interface Dave {
+  /**
+   * count the number of lines in a file
+   */
+  int countLines(File inFile); // not the real signature!
+}"
+
+loc_count = 0
+#str.chars.each { |chr| loc_count += 1 if chr == "\n" }
+str.lines.each { |line| loc_count += 1 if !line.strip.start_with?("//", "*/", "/*") }
+puts loc_count
