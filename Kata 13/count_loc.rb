faad8b6679a4b91bba7b57d7bacf9c81433257fc
dibikhin <dibikhin@yahoo.com>
2014-09-02 14:25:19 +0400

    counted loc ok

--- Kata 13/count_loc.rb
+++ Kata 13/count_loc.rb
@@ -1,14 +1,35 @@
-﻿str = "// This file contains 3 lines of code
-public interface Dave {
+﻿def delete_block_comments(str)
+	str.gsub(/\/\*(\n|.)*?\*\//, '')   # '?' is ungreedy quantifier and does all work 
+end
+
+def count_loc(str)
+	loc_count = 0
+	str_wo_block_comments = delete_block_comments(str)
+	puts str_wo_block_comments
+	str_wo_block_comments.lines.each { |line| loc_count += 1 if !line.strip.start_with?("//") && line.strip != ''  }
+	loc_count
+end
+
+simple_comments = ' // This file contains 3 lines of code
+ public interface Dave {
   /**
    * count the number of lines in a file
    */
   int countLines(File inFile); // not the real signature!
-}"
+}'
 
-loc_count = 0
-#str.chars.each { |chr| loc_count += 1 if chr == "\n" }
-str.lines.each { |line| loc_count += 1 if !line.strip.start_with?("//", "*/", "/*") }
-puts loc_count
+complex_comments = ' /*****
+  * This is a test program with 5 lines of code
+  *  \/* no nesting allowed!
+  //*****//***/// Slightly pathological comment ending...
 
-#.gsub(/\/\*(\n|.)*\*\//, '').strip
+ public class Hello {
+    public static final void main(String [] args) { // gotta love Java
+        // Say hello
+      System./*wait*/out./*for*/println/*it*/("Hello/*");
+    }
+
+}'
+
+puts count_loc(simple_comments)
+puts count_loc(complex_comments)
