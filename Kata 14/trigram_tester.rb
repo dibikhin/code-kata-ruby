528cfff407fa645f815fbf54d321e28dcd3f6427
dibikhin <dibikhin@yahoo.com>
2014-09-03 22:16:22 +0400

    trigram composition done, tests failed

--- Kata 14/trigram_tester.rb
+++ Kata 14/trigram_tester.rb
@@ -1,7 +1,7 @@
 ﻿require 'pp'
 
 def split_and_clear_text(txt)
-#	%w(I wish I may I wish I might)
+	# %w(I wish I may I wish I might)
 	txt.split
 end
 
@@ -23,17 +23,22 @@ def find_trigrams(txt)
 	trigrams
 end
 
-def compose_story_recur(trigrams, new_story, depth, max_depth)
+def compose_story_recur(trigrams, current_key, new_story, depth, max_depth)
+	depth += 1
 	return new_story if depth > max_depth
+	trigram_values = trigrams[current_key]
+	new_story << "%s %s %s " % [current_key[0], current_key[1], trigram_values[0]]
+	compose_story_recur(trigrams, current_key, new_story, depth, max_depth)
 end
 
 def compose_story(trigrams)
 	# pp trigrams
 	new_story, depth, max_depth = '', 0, 10
-	compose_story_recur(trigrams, new_story, depth, max_depth)
+	starting_key = trigrams.keys[Random.new.rand(trigrams.keys.size - 1)]
+	compose_story_recur(trigrams, starting_key, new_story, depth, max_depth)
 	new_story
 end
 
 txt = load_text('sample.txt')
 trigrams = find_trigrams(txt)
-compose_story(trigrams)
+puts compose_story(trigrams)
