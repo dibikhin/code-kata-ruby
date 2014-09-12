1f50ad046e1f259a1abea55315fd154b24aca5e8
dibikhin <dibikhin@yahoo.com>
2014-09-12 21:37:51 +0400

    kata 15 draft

---
+++ Kata 15/test_adjacent_bits.rb
@@ -0,0 +1,18 @@
+﻿require 'minitest/autorun'
+require 'pp'
+
+def find_adjacent_bits(bit_count)
+	adjacent_bits_nums = []
+	(0..(2 ** bit_count - 1)).each do |num| 
+		(0..(bit_count - 2)).each do |shift|
+			adjacent_bits_nums << num if num == num | (1 << shift)
+		end
+	end
+	adjacent_bits_nums
+end
+
+class TestRack < Minitest::Test
+	def test_three_digit_bins
+		assert_equal([0b011, 0b110], find_adjacent_bits(3))
+	end
+end
